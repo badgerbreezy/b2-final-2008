845da8564e382015492e5c9df1777af77f477c45
Brian Liu <brian.b.liu@gmail.com>
2020-11-09 11:09:43 -0700

    Remove edit route from doctor, unecessary atm.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: [:show, :edit, :update]
+  resources :doctors, only: [:show, :update]
   resources :hospitals, only: [:show]
 end
