4f2e60d7e7809c6f005dd26a0041da4c0d02461a
Brian Liu <brian.b.liu@gmail.com>
2020-11-09 11:08:18 -0700

    Add update action in doctors controller to remove patient.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,11 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
+
+  def update
+    @doctor = Doctor.find(params[:id])
+    @patient = @doctor.patients.find(params[:patient_id])
+    @patient.update(doctor_id: nil)
+    redirect_to "/doctors/#{@doctor.id}"
+  end
 end
